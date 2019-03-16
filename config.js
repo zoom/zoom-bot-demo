7d64adb3783aa40ccbc4c53ab0974dfbef361af6
Ojus Save <ojus.save@zoom.us>
2019-03-15 19:25:45 -0700

    new bot credentials

--- config.js
+++ config.js
@@ -3,14 +3,16 @@ const env = process.env.NODE_ENV || 'development'
 //insert your API Key & Secret for each environment, keep this file local and never push it to repo.
 const config = {
 	development :{
-		clientID : 'nKZmSkNSSXOxFcVCtB0jaA',
-		clientSecret : 'cI9PraeqEXzNM0mSYpIh7LHWW9kkJurQ',
-		redirectUrl : 'https://bb7fe819.ngrok.io',
-		verifyCode : 'wtIceovcROe68qBa_NsDgQ',
-		botJid : 'v1prfqdonqtjw9e4urow4bca@xmppdev.zoom.us',
-		botName : 'OjusNewBotTest',
-		to_jid : '88f148d43f0141f6a2901225ef6f5e49@conference.xmppdev.zoom.us',
-		accountId : 'Uoj5nh68RtK_C737r6XTIw'
+		clientID : 'MUqsqBxxT7mF4nuaJ2Ua8Q',
+		clientSecret : 'aw2Mv4EZ6e00dZ3j4aCgoo2b8x3eWvp1',
+		redirectUrl : 'http://8d10eb12.ngrok.io/oauth',
+		redirect_uri: 'http://8d10eb12.ngrok.io/oauth',
+		verifyCode : 'PBBR9irlQHGVWSg5qJEbGQ',
+		botJid : 'v11_dokxygrn2p0aqf4-dv7q@xmpp.zoom.us ',
+		botName : 'ZoomChatbot1',
+		to_jid : 'jqfut1woqra0opxcyeanfq@xmpp.zoom.us',
+		account_id : 'J5yuBxbFT0iKHvOh1iR3Rg',
+
 
 	},
 	production:{
