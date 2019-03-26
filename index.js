137811777c92b08b7e536d4d4f456742fd69994c
Ojus Save <ojus.save@zoom.us>
2019-03-26 13:36:24 -0700

    user has to add the values, help section updated

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const config = require('./config');
 var express = require('express');
 let http = require('http');
 var bodyParser = require("body-parser");
+var readline = require('readline-sync');
+
+
 let app = express();
 //app.use(express.static('./test'));
 app.use(bodyParser.json());
@@ -47,10 +50,10 @@ let zoomBot = client(
   config.clientID,//client id
   config.verifyCode,//verify code
   config.botJid,//jid
-  config.botName).commands([{ command: 'Actions :', description: 'When you enter the "Actions" command, the bot displays two buttons' },
-{ command: 'Links :', description: 'When you enter this command you will see an example with clickable links' },
-{ command: 'Fields :',  description: 'When you enter this command you will see an example with two fields ' },
-{ command: 'Cards :',  description: 'When you enter this command you will see three types of examples: \n - a simple message, \n - a message with a clickable link and  \n - a message with style' }]).defaultAuth(oauth2Client.connect());
+  config.botName).commands([{ command: 'Actions :', description: 'When you enter the "Actions" command, the bot displays two buttons, \nSyntax: actions "title" "button1" "button2"  ' },
+{ command: 'Links :', description: 'When you enter this command you will see an example with clickable links, \nSyntax: links "title" "text" "link"' },
+{ command: 'Fields :',  description: 'When you enter this command you will see an example with two fields, \nSyntax: fields "title" "key 1" "value 1" "key 2" "value 2" ' },
+{ command: 'Cards :',  description: 'When you enter this command you will see three types of examples: \n - a simple message, \n - a message with a clickable link and  \n - a message with style, \nSyntax: cards "title" "text for simple message" "text for link" "link" "text for style"' }]).defaultAuth(oauth2Client.connect());
 
 
 
@@ -69,13 +72,18 @@ zoomBot.on('commands', function (e) {
 
   //var nJSON = function(e);
 
-console.log(data);  // else if(command==='delete'){//other command logic}
+//console.log(command, text);  // else if(command==='delete'){//other command logic}
  if (command === "actions") {
 
     console.log("we are within Actions");
 
-  reqBody =  [{type: 'actions', limit:'2', items:[{text: 'update', event_id:'update', event:'sendMsg(\"/weather update\")'},{text: 'delete', event_id:'delete', event:'sendMsg(\"/weather delete\")'}]}];
-  reqHeader = { text: 'This is the title for Actions' };
+
+  let text = type === 'group' ? `Your Title is ${data[0]}` : `Your Title is ${data[0]}`;
+  let text1 = type === 'group' ? `${data[1]}` : `${data[1]}`;
+  let text2 = type === 'group' ? `${data[2]}` : `${data[2]}`;
+
+  reqBody =  [{type: 'actions', limit:'2', items:[{text: text1, event_id:'update', event:'sendMsg(\"/weather update\")'},{text:text2 , event_id:'delete', event:'sendMsg(\"/weather delete\")'}]}];
+  reqHeader = { text: text};
 
   reqBody1 = { type: 'message', text:myJSON };
   reqHeader1 = { text: 'Here is JSON for Actions'};
@@ -106,8 +114,12 @@ console.log(data);  // else if(command==='delete'){//other command logic}
     console.log("we are in links");
 
 
-      reqBody = [{ type: 'message', text: 'Zoom Video', link: 'www.zoom.us'}];
-      reqHeader ={ text: `This is the title for links` };
+    let text = type === 'group' ? `Your Title is ${data[0]}` : `Your Title is ${data[0]}`;
+    let text1 = type === 'group' ? `${data[1]}` : `${data[1]}`;
+    let text2 = type === 'group' ? `${data[2]}` : `${data[2]}`;
+
+      reqBody = [{ type: 'message', text: text1, link: text2}];
+      reqHeader ={ text: text };
 
       reqBody1 = { type: 'message', text:myJSON };
       reqHeader1 = { text: 'Here is JSON for Links'};
@@ -121,9 +133,15 @@ console.log(data);  // else if(command==='delete'){//other command logic}
 
     console.log("we are within fields");
 
+    let text = type === 'group' ? `Your Title is ${data[0]}` : `Your Title is ${data[0]}`;
+    let text1 = type === 'group' ? `${data[1]}` : `${data[1]}`;
+    let text2 = type === 'group' ? `${data[2]}` : `${data[2]}`;
+    let text3 = type === 'group' ? `${data[3]}` : `${data[3]}`;
+    let text4 = type === 'group' ? `${data[4]}` : `${data[4]}`;
 
-    reqBody = [{type: 'fields', items:[{key:'Zoom Video Communications', value: 'Zoom'}, { key:'San Jose, California', value: 'SJC'}]}];
-    reqHeader = { text: `This is the title for Form fields` };
+
+    reqBody = [{type: 'fields', items:[{key:text1, value: text2}, { key:text3, value: text4}]}];
+    reqHeader = { text: text };
 
     reqBody1 = { type: 'message', text:myJSON };
     reqHeader1 = { text: 'Here is JSON for Fields'};
@@ -138,8 +156,15 @@ console.log(data);  // else if(command==='delete'){//other command logic}
 
     console.log("we are in multiple");
 
-    reqBody = [{ type: 'message', text: 'This is a simple message'}, { type: 'message', text: 'This is message with a link', link: 'https://zoom.us'}, {type: 'message', text: 'This is a message with style', style: {color: '#ff0000', bold: 'true'}}];
-    reqHeader = { text: `This the title for multiple messages` };
+    let text = type === 'group' ? `Your Title is ${data[0]}` : `Your Title is ${data[0]}`;
+    let text1 = type === 'group' ? `${data[1]}` : `${data[1]}`;
+    let text2 = type === 'group' ? `Your message is ${data[2]}, and your link is ${data[3]}` : `Your message is ${data[2]}, and your link is ${data[3]}`;
+    let text3 = type === 'group' ? `${data[3]}` : `${data[3]}`;
+    let text4 = type === 'group' ? `${data[4]}` : `${data[4]}`;
+
+
+    reqBody = [{ type: 'message', text: text1}, { type: 'message', text: text2, link: text3}, {type: 'message', text: text4, style: {color: '#ff0000', bold: 'true'}}];
+    reqHeader = { text: text };
 
     reqBody1 = { type: 'message', text:myJSON };
     reqHeader1 = { text: 'Here is JSON for Multiple Messages'};
@@ -197,7 +222,26 @@ app.all('/message',function (req, res) {//get message api
 }
 });
 
-
+app.all('/message', async function (req, res) {//get message api
+  try{
+    let { body, headers } = req;
+  //i will add help,
+  let info=zoomBot.parse({body,headers});
+  if(info.status===true){
+    let {command,payload,message,type,event}=info.data;
+    try{
+  zoomBot.handle({ body, headers }, function (err) {
+    if (err) { console.log(err, "this is an error with /message"); }
+    res.send('ok');
+  });
+} catch (e){
+  res.send(e);
+}
+}
+} catch (e){ //
+  res.send(e);
+}
+});
 
 
 app.get('/oauth', async function (req, res) {
